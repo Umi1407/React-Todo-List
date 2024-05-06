9b7b76ad6401bfccc1312a264f2ed77b11ef2a70
Umi Sato <UmiSato42@gmail.com>
2024-05-06 14:31:25 -0400

    Files add

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import "./App.css"
+import TodoList from './TodoList';
+import TodoInput from './TodoInput';
 
 function App() {
+  const [todo, setTodo] = useState("");
+  const [todos, setTodos] = useState([]);
+
+  const changeColor = () => {
+    if (document.body.style.backgroundColor === 'black') {
+      document.body.style.backgroundColor = 'bisque';
+    } else {
+      document.body.style.backgroundColor = 'black';
+    }
+  }
+  const addTodo = () => {
+    if (todo !== "") setTodos([...todos, todo]);
+    setTodo("");
+  }
+  const deleteTodo = (text) => {
+    const newTodos = todos.filter((todo) => {
+      return todo !== text;
+    });
+    setTodos(newTodos);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button className='light-button' onClick={changeColor}>Toggle light mode</button>
+      <div className="App">
+        <h1>Todo App</h1>
+        <TodoInput todo={todo} setTodo={setTodo} addTodo={addTodo} />
+        <TodoList list={todos} remove={deleteTodo} />
+      </div>
     </div>
   );
 }
